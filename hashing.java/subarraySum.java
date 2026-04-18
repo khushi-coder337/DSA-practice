6b0225db394e0bd4aed8854ba97d0965e600b1c8
Khushi <sunilsingh1191981@gmail.com>
2026-04-18 13:02:39 +0530

    subarrays sum equals k

---
+++ hashing.java/subarraySum.java
@@ -0,0 +1,18 @@
+class Solution {//t.c -> O(n)
+    public int subarraySum(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //sum,count
+        map.put(0,1);
+        int sum = 0;
+        int ans = 0;
+
+        for(int j=0; j<arr.length; j++){
+            sum += arr[j];
+            if(map.containsKey(sum-k)){
+                ans += map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
+}
