8871faa9dda056c7fef1a07f63bf77bea84e374f
Khushi <sunilsingh1191981@gmail.com>
2026-04-19 17:19:34 +0530

    Add trie implementation for longest word search

---
+++ tries/longestWord.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class basic{
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+    
+        public Node(){
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+    public static Node root = new Node();
+    
+    public static void insert(String word){
+        Node curr = root;
+        for(int level = 0; level<word.length(); level++){
+            int idx  = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+     public static boolean search(String key){
+        Node curr = root;
+        for(int level = 0; level<key.length(); level++){
+            int idx  = key.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+  
+    public static String ans = "";
+    public static void longestWord(Node root, StringBuilder temp){
+        if(root == null){
+            return;
+        }
+        for(int i=0; i<26; i++){
+            if(root.children[i] != null && root.children[i].eow == true){
+                char ch = (char)(i + 'a');
+                temp.append(ch);
+                if(temp.length() > ans.length()){
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i],temp);
+                temp.deleteCharAt(temp.length()-1);//backtrack
+            }
+        }
+    }
+    public static void main(String args[]){
+         String word[] = {"banana","a","ban","ap","app","apple","appl","apply"};
+        for(int i=0; i<word.length; i++){
+            insert(word[i]);
+        }
+        longestWord(root,new StringBuilder(""));
+        System.out.println(ans);
+    }
+}
