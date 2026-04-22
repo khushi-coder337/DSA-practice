96c02458cf2aff45ac8d0279cb679522f50adfb5
Khushi <sunilsingh1191981@gmail.com>
2026-04-22 20:00:20 +0530

    Implement cycle detection in undirected graph

---
+++ graph/undirectedCycle.java
@@ -0,0 +1,72 @@
+import java.util.*;
+public class detectCycle{//t.c ->O(V+E)
+    static class Edge{
+        int src;
+        int dest;
+        
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+            
+        }
+    }
+     public static void createGraph(ArrayList<Edge>[] graph){
+          for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        //0 vetex
+        graph[0].add(new Edge(0,1));
+        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,3));
+        //1 vertex
+          graph[1].add(new Edge(1,0));
+          graph[1].add(new Edge(1,2));
+          
+          //2 vertex
+            graph[2].add(new Edge(2,0));
+            graph[2].add(new Edge(2,1));
+           
+            //3 vertex
+                 graph[3].add(new Edge(3,0));
+                 graph[3].add(new Edge(3,4));
+                //4th vertex
+                    graph[4].add(new Edge(4,3));
+                   
+     }
+
+     public static boolean detectCycle(ArrayList<Edge>[] graph){
+        boolean[] vis = new boolean[graph.length];
+        for(int i=0; i<graph.length; i++){
+            if(!vis[i]){
+                if(detectCycleUtil(graph,vis,i,-1)){
+                    return true;//cycle exists in one of the part
+                }
+            }
+        }
+        return false;
+     }
+     public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[]vis, int curr, int par){
+        vis[curr] = true;
+        for(int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            //case 3 :
+            if(!vis[e.dest]){
+                if(detectCycleUtil(graph,vis,e.dest,curr)){
+                     return true;
+                }
+               
+                //case 1
+            }else if(vis[e.dest] && e.dest != par){
+                return true;
+            }
+            //else- case 2 -> continue - do nothing
+        }
+        return false;
+     }
+    public static void main(String args[]){
+            int V = 5;
+            ArrayList<Edge>[] graph = new ArrayList[V];
+            createGraph(graph);
+            System.out.println(detectCycle(graph));
+    }
+}
