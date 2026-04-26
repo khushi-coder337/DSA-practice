b632a82e1c47336aa8be4098adc716531f83c965
Khushi <sunilsingh1191981@gmail.com>
2026-04-26 15:37:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -0,0 +1,41 @@
+class Solution {
+    static class Pair implements Comparable<Pair>{
+        int v;
+        int cost;
+        public Pair(int v, int cost){
+            this.v = v;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Pair p2){
+            return this.cost - p2.cost;
+        }
+    }
+    public int spanningTree(int V, int[][] edges) {
+     boolean[] vis = new boolean[V];
+     PriorityQueue<Pair> pq = new PriorityQueue<>();
+     pq.add(new Pair(0,0));
+     int finalCost = 0; //min. weight
+     while(!pq.isEmpty()){
+         Pair curr = pq.remove();
+         if(!vis[curr.v]){
+             vis[curr.v] = true;
+             finalCost += curr.cost;
+             
+             for (int i = 0; i < edges.length; i++) {
+                int u = edges[i][0];
+                int v = edges[i][1];
+                int wt = edges[i][2];
+
+     // If the edge connects to our current vertex, add the other side to PQ
+                if (u == curr.v) {
+                    pq.add(new Pair(v, wt));
+                 } else if (v == curr.v) {
+                     pq.add(new Pair(u, wt));
+                  }
+                }
+         }
+     }
+     return finalCost;
+    }
+}
