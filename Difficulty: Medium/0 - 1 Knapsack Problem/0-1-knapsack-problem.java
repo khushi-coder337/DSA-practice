5db6e9dac4c24e1e834c1e1ffbb58eca5a9677bb
Khushi <sunilsingh1191981@gmail.com>
2026-05-03 23:55:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,29 +1,31 @@
 class Solution {
-    public int knap(int W, int val[], int wt[], int n, int[][] dp){//memoisation
-        //base case
-        if(W == 0 || n == 0){
-            return 0;
-        }
-        if(dp[n][W] != -1){
-            return dp[n][W];
-        }
-        if(wt[n-1] <= W){//valid
-            int ans1 = val[n-1] + knap(W-wt[n-1], val, wt,n-1,dp);//include
-            int ans2 = knap(W,val,wt,n-1,dp);//exclude
-            dp[n][W] =  Math.max(ans1,ans2);
-            return dp[n][W];
-        }else{//not valid
-            dp[n][W] =  knap(W,val,wt,n-1,dp);
-            return dp[n][W];
-        }
-    }
-    public int knapsack(int W, int val[], int wt[]) {
-        int[][] dp = new int[val.length+1][W+1];
+    static int rec(int W, int val[], int[] wt){
+        int n = val.length;
+        int[][] dp = new int[n+1][W+1];
         for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
-                dp[i][j] = -1;
+            dp[i][0] = 0;//items row
+        }
+        for(int j=0; j<dp[0].length; j++){
+            dp[0][j] = 0;//weight col
+        }
+        //tabulation code
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<W+1; j++){
+                int v = val[i-1]; //item val
+                int w = wt[i-1]; // weight val
+                if(w <= j){//valid
+                    int include = v + dp[i-1][j-w];
+                    int exclude = dp[i-1][j];
+                    dp[i][j] = Math.max(include,exclude);
+                }else{//include
+                    int exclude = dp[i-1][j];
+                    dp[i][j] = exclude;
+                }
             }
         }
-       return knap(W,val,wt,val.length,dp);
+        return dp[n][W];
+    }
+    public int knapsack(int W, int val[], int wt[]) {
+        return rec(W,val,wt);
     }
 }
